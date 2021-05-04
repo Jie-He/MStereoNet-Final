b690b461e14e5e7ef64fe2fadf4a85486d7eaef0
Jie He <Jiehe1998yd@gmail.com>
2021-05-05 00:57:25 +0100

    Remove local path EPE folder creation

--- dataset_paths.py
+++ dataset_paths.py
@@ -5,10 +5,12 @@ data_paths_local = {'mscoco':     'sample_datasets/coco_test/', ## local
                     #'mscoco':     '/mnt/seaweed/homes/jh2699/Project/MDataset',
                     'middlebury': 'D:/StereoTrainingSet/Middlebury_Rescale',
                     'kitti15':    'D:/StereoTrainingSet/KITTI_2015/training',
-                    'kitti12':    'D:/StereoTrainingSet/KITTI_2012/training'}
+                    'kitti12':    'D:/StereoTrainingSet/KITTI_2012/training',
+                    'EPE_path':   'D:/StereoTrainingSet/EPE/'}
 
 ## Sample paths
 data_paths = {'mscoco':     'sample_datasets/coco_test/',
               'middlebury': 'sample_datasets/middlebury_Rescale',
               'kitti15':    'sample_datasets/KITTI_2015/training',
-              'kitti12':    'sample_datasets/KITTI_2012/training'}
+              'kitti12':    'sample_datasets/KITTI_2012/training',
+              'EPE_path':   'sample_datasets/EPE/'}
